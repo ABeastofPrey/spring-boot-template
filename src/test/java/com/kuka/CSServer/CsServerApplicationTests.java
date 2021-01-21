33e7fd53ddbc72f8a0fa331835a971ea3d9ed0e3
Wenbo.Wang1 <wenbo.wang1@kuka.com>
2021-01-21 11:38:34 +0800

    add unit test

--- src/test/java/com/kuka/CSServer/CsServerApplicationTests.java
+++ src/test/java/com/kuka/CSServer/CsServerApplicationTests.java
@@ -1,13 +1,54 @@
 package com.kuka.CSServer;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CsServerApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@BeforeAll
+	public static void setAll() {
+		System.out.println("Set all");
+	}
+	
+	@BeforeEach
+	public void setUp() {
+		System.out.println("Set up");
 	}
 
+	@AfterEach
+	public void tearDown() {
+		System.out.println("Tear down");
+	}
+
+	@AfterAll
+	public static void tearAll() {
+		System.out.println("Tear all");
+	}
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+	@Disabled
+	@Test
+	void contextLoads1() {
+		assertTrue(false);
+	}
+
+	@Test
+	void testNegative() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			throw new IllegalArgumentException();
+		});
+	}
 }
