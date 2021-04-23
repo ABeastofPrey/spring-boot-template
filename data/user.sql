feb35b638eea0965cd30f3c5e19b21fb57beded5
WenBo <wenbo.wang1@kuka.com>
2021-04-23 15:57:08 +0800

    loggin with db user

--- data/user.sql
+++ data/user.sql
@@ -7,13 +7,13 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
     `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(50) NOT NULL,
-    `password` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(60) NOT NULL,
     `phone` BIGINT(11),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='User table';
 
 INSERT INTO `users` (`username`, `password`, `phone`) VALUES 
-('USER 0', '123456', 13585845436),
+('USER 0', '$2a$10$EmsokMb6Vkav7m61kY0PtO.ZCLe0h.uJqVAZW7YYBpSUxd/DMkZuG', 13585845436), -- password is crypted from 123456 by BCryptPasswordEncoder
 ('USER 1', 'admin1', 13585845432), 
 ('USER 2', 'admin2', 13584854321), 
 ('USER 3', 'admin3', 13884832812);
--- src/main/java/com/kuka/springtemplate/common/auth/AuthUserService.java
+++ src/main/java/com/kuka/springtemplate/common/auth/AuthUserService.java
@@ -1,7 +1,9 @@
 package com.kuka.springtemplate.common.auth;
 
 import com.kuka.springtemplate.model.User;
+import com.kuka.springtemplate.service.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthUserService implements UserDetailsService {
+	@Autowired UserService userService;
     // 加载用户信息
 	@Override
 	public UserDetails loadUserByUsername(String account) throws UsernameNotFoundException {
@@ -17,7 +20,11 @@ public class AuthUserService implements UserDetailsService {
 		// password的值：$2a$10$EmsokMb6Vkav7m61kY0PtO.ZCLe0h.uJqVAZW7YYBpSUxd/DMkZuG，
 		// 是明文123456使用BCryptPasswordEncoder加密的值
 		// User user = new User(1l, "abc1", username, "$2a$10$EmsokMb6Vkav7m61kY0PtO.ZCLe0h.uJqVAZW7YYBpSUxd/DMkZuG", "admin");
-		User user = new User(1l, account, "$2a$10$EmsokMb6Vkav7m61kY0PtO.ZCLe0h.uJqVAZW7YYBpSUxd/DMkZuG", "13585845432");
+		
+		// User user = new User(1l, account, "$2a$10$EmsokMb6Vkav7m61kY0PtO.ZCLe0h.uJqVAZW7YYBpSUxd/DMkZuG", "13585845432");
+
+		User user = userService.findByPhone(account);
+
 		AuthUser authUser = new AuthUser(user);
 		
 		return (UserDetails) authUser;
